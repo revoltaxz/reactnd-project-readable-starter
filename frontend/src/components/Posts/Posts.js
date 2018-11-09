946b9394f0a2c1a3ca606d3e189c1513e89c9fcf
Guilherme Revolta <guilhermerevolta@gmail.com>
2018-11-09 14:18:45 -0200

    refactor: post style

--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -1,18 +1,17 @@
 import React from 'react'
-
 import { connect } from 'react-redux'
 import { getAllPosts, deletePost, vote, getPostByCategory, editPost } from '../../actions/posts';
 import { bindActionCreators } from 'redux'
 import DeleteIcon from '@material-ui/icons/Delete'
 import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
 import { withStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
-import InfoLabel from '../InfoLabel/InfoLabel'
-import Like from '@material-ui/icons/ThumbUp'
-import Deslike from '@material-ui/icons/ThumbDown'
+import Up from '@material-ui/icons/KeyboardArrowUp'
+import Down from '@material-ui/icons/KeyboardArrowDown'
 import EditPost from "./EditPost";
 
 class Posts extends React.Component {
@@ -43,32 +42,22 @@ class Posts extends React.Component {
               <Card>
                 <CardContent>
                   <Grid container>
-                    <Grid item xs={9} style={{ marginTop: 12}}>
-                      <Typography variant="title">{post.title}</Typography>
-                      <Typography variant="caption">{post.author}</Typography>
+                    <Grid item xs={1}>
+                      <Grid container direction="column" alignItems="flex-start">
+                        <Button onClick={() => vote(post, 'upVote' )}><Up /></Button>
+                        <span style={{ paddingLeft: 25.5 }}>{post.voteScore}</span>
+                        <Button onClick={() => vote(post, 'downVote' )}><Down /></Button>
+                      </Grid>
                     </Grid>
-                    <Grid item xs={3}>
+                    <Grid item xs={9} style={{ marginTop: 12}}>
+                      <Typography variant="subtitle1">{post.title} | {post.category} </Typography>
+                      <Typography variant="caption">submitted by {post.author} | {post.commentCount} comments</Typography>
+                    </Grid>
+                    <Grid item xs={2}>
                       <IconButton style={{float: 'right'}} onClick={() => this.props.deletePost(post)}><DeleteIcon /></IconButton>
                       <EditPost {...post} onSubmit={this.edit} form={`form-edit-${post.id}`}/>
                     </Grid>
                   </Grid>
-                </CardContent>
-                <CardContent>
-                  <Grid container>
-                    <Grid item xs={6}>
-                      <InfoLabel label="Category" data={post.category} />
-                    </Grid>
-                    <Grid item xs={6}>
-                      <InfoLabel label="Vote Score" data={post.voteScore} />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <InfoLabel label="Body" data={post.body} />
-                    </Grid>
-                  </Grid>
-                </CardContent>
-                <CardContent>
-                  <IconButton style={{ float: 'right' }} onClick={() => vote(post, 'upVote' )}><Like /></IconButton>
-                  <IconButton style={{ float: 'right' }} onClick={() => vote(post, 'downVote' )}><Deslike /></IconButton>
                 </CardContent>
               </Card>
             </Grid>
