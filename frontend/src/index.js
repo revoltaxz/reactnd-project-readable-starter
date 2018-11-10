fbe7f1508329cd129b559b5d543304f2e2b47ab8
Guilherme Revolta <guilhermerevolta@gmail.com>
2018-11-10 00:09:21 -0200

    fix: store params

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import  rootReducer  from './reducers'
 import thunkMiddleware from 'redux-thunk';
 import multi from 'redux-multi'
 import { createLogger } from 'redux-logger'
 
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const loggerMiddleware = createLogger()
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = createStore(rootReducer, devTools, applyMiddleware(thunkMiddleware,loggerMiddleware, multi))
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunkMiddleware ,loggerMiddleware, multi)))
 
 ReactDOM.render(
   <Provider store={store}>
