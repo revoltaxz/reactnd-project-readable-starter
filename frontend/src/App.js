e3ad403faf8a07d8f1774d957151b89abc314848
Guilherme Revolta <guilhermerevolta@gmail.com>
2018-11-12 22:27:23 -0200

    refactor: App component to stateless component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Posts from './components/Posts/Posts'
 import Header from './components/Header/Header'
@@ -7,21 +7,18 @@ import { history } from "./utils/history";
 import PostDetail from "./components/PostDetail/PostDetail";
 import NoRoute from "./components/NoRoute/NoRoute";
 
-class App extends Component {
-  render() {
-    return (
-      <Header>
-        <Router history={history}>
-          <Switch>
-            <Route exact path="/" component={Posts} />
-            <Route exact path="/category/:category" component={Posts} />
-            <Route exact path="/category/:category/:post_id" component={PostDetail} />
-            <Route component={NoRoute} />
-          </Switch>
-        </Router>
-      </Header>
-    );
-  }
-}
+
+  const App = () => (
+    <Header>
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={Posts} />
+          <Route exact path="/:category" component={Posts} />
+          <Route exact path="/:category/:post_id" component={PostDetail} />
+          <Route component={NoRoute} />
+        </Switch>
+      </Router>
+    </Header>
+  )
 
 export default App;
